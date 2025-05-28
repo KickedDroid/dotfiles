b1d64bea1934315172e96fa1aeff730bfbb3a37d
KickedDroid <24928676+KickedDroid@users.noreply.github.com>
2025-05-28 11:41:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@ echo "[+] Starting Setup ...";
 
 sudo apt install i3status dmenu &&
 
-echo "[+] i3 installed";
+echo "[+] i3 installed, setting up config";
+
+touch ~/.config/i3/config;
+sudo curl -o ~/.config/i3/config https://raw.githubusercontent.com/KickedDroid/dotfiles/refs/heads/main/i3/config &&
+
+echo "[+] i3 configured"; 
 
