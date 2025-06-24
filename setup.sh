503500155e6d4c8bddfc18a1722ea24bd019f117
KickedDroid <24928676+KickedDroid@users.noreply.github.com>
2025-06-24 14:59:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,23 @@ cargo install rustscan;
 
 cargo install bottom; 
 
-sudo apt install alacritty
+sudo apt install alacritty;
+
+
+mkdir ~/Documents/tools & cd ~/Documents/tools;
+
+git clone https://github.com/arthaud/git-dumper.git;
+git clone https://github.com/micahvandeusen/gMSADumper.git;
+git clone https://github.com/urbanadventurer/username-anarchy.git;
+git clone https://github.com/dirkjanm/PKINITtools.git;
+git clone https://github.com/ShutdownRepo/pywhisker.git;
+git clone https://github.com/ShutdownRepo/targetedKerberoast.git;
+wget -o https://github.com/KickedDroid/ls-ad-users/raw/refs/heads/master/ls-ad-users;
+git clone https://github.com/jalvarezz13/Krb5RoastParser.git;
+
+
+
+
 
 
 
